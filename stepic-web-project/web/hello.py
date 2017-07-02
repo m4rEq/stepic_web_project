5ffd4ce39f44b335d2b68d9b55740e0960037128
Marat Kashaev <kashaevmt@gmail.com>
2017-07-02 14:45:12 +0400

    change body

--- stepic-web-project/web/hello.py
+++ stepic-web-project/web/hello.py
@@ -5,9 +5,8 @@ def wsgi_application(environ, start_response):
 	headers = [
 		('Content-Type', 'text/plain')
 	]
-	body = ''
-	for i in environ:
-		body += str(i) + '\n'
+	
+	body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
 	
 	start_response(status, headers)
-	return [body]
+	return body
